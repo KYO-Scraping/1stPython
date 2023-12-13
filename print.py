6182b4ec25fcdf76a251e6c5a64886696f95b1a4
kevinkandou <focus.kandou@gmail.com>
2023-12-13 18:54:26 +0800

    edit 2nd

--- print.py
+++ print.py
@@ -1,6 +1,11 @@
 Apple_Weight = 8
 Apple_Count = 5
-
+"""
 print(f"Apple Weight = {Apple_Weight} gram")
 print("Apple Count =", Apple_Count)
 print("Total Weight =", Apple_Weight * Apple_Count, "gram")
+"""
+
+T = 'mango'
+
+print(T[0:3])
